22a64afa56c3301f306ab5d7fefb4ad14f367df6
sandeep dommaraju <sandeepdommaraju@gmail.com>
2016-04-17 23:30:31 -0400

    step 1: initial handshake is working; start step 2: send bitfield messages

--- src/Peer.java
+++ src/Peer.java
@@ -1,92 +1,220 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
- * Created by Ash on 3/13/16.
+ * Created by sunito on 4/17/16.
  */
+
+
 public class Peer implements Runnable{
+
     private int id;
-    private int numNeighbours;
-    private BitField bitField;
+    private int idx;
+    private int N;
+    private int K;
+    private int p;
+    private int m;
+    private int noOfPieces;
+
+    private int[] peerids;
+    private Map<Integer, BitSet> bitfieldsMap;
+    private Map<Integer, Boolean> handShakeMap;
+
     private P2PLogger peerLog;
     private Configuration config;
-    private ServerSocket serverSocket;
-    HashMap<Integer, Boolean> handShakeMsgs = new HashMap<Integer, Boolean>();
 
-    public Peer(int id) {
+    public Peer(int id){
+        config = new Configuration("Common.cfg", "PeerInfo.cfg");
+
         this.id = id;
-        this.config = new Configuration("Common.cfg", "PeerInfo.cfg");
-        this.peerLog = new P2PLogger(this.id);
-        this.bitField = new BitField(config.getTotalNumOfPieces());
-        this.numNeighbours = config.getPrefNeighboursCount();
+        N = config.getTotalPeers();
+        K = config.getPrefNeighboursCount();
+        p = config.getUnchokingInterval();
+        m = config.getOptUnchokingIntervl();
+        noOfPieces = config.getTotalNumOfPieces();
+
+        peerids = new int[N];
+        bitfieldsMap = new HashMap<>();
+        handShakeMap = new HashMap<>();
+
+        peerLog = new P2PLogger(this.id);
     }
 
     @Override
-    public void run() {
-        System.out.println("Peer " + this.id + " has entered the Bit Torrent connection");
+    public void run(){
 
-        //Checking if the peer has complete file
-        for (int i = 0; i < config.getTotalPeers(); i++) {
-            if (config.getPeerID().get(i) == this.id) {
-                //Has all the pieces of the file
-                if (config.getHaveFile().get(i)) {
-                    //Setting all its bit fields to true
-                    this.bitField.setBitsToTrue();
-                }
+        System.out.println("Started running peer: " + id);
+
+        List<String> hosts = config.getHostName();
+        List<Integer> ports = config.getPortNumber();
+        List<Integer> ids = config.getPeerID();
+        for(int i=0; i<N; i++) {
+            peerids[i] = ids.get(i);
+            if(peerids[i] == id){
+                idx = i;
             }
         }
 
-        //Starting the server socket connection for the peer
-        try {
-            for (int i = 0; i < config.getTotalPeers(); i++) {
-                if (config.getPeerID().get(i) == this.id) {
-                    this.serverSocket = new ServerSocket(config.getPortNumber().get(i));
-                    break;
-                }
-                //Creating client socket with other Server Sockets currently active
-                Socket clientSocket = new Socket(config.getHostName().get(i), config.getPortNumber().get(i));
-                Calendar cal = Calendar.getInstance();
-                this.peerLog.logTcpConnection(config.getPeerID().get(i), cal);
-//                System.out.println(clientSocket.)
-
-                //Send Handshake messages
-                Handshake handshakeFrmClient = new Handshake();
-                handshakeFrmClient.setPeerId(this.id);
-                handshakeFrmClient.sendHandshakeMsg(clientSocket);
-                handShakeMsgs.put(config.getPeerID().get(i), false);
-                handshakeFrmClient.handShakeReceived(clientSocket);
-                System.out.println("Handshake initiation received from " + handshakeFrmClient.getPeerId());
-                if (handShakeMsgs.get(handshakeFrmClient.getPeerId()) != null) {
-                    handShakeMsgs.put(handshakeFrmClient.getPeerId(), true);
-                    peerLog.logHandshakeSuccess(handshakeFrmClient.getPeerId(),Calendar.getInstance());
-                    System.out.println("Handshake Success");
-                }
-            }
-            while (true) {
-                Socket serverS = serverSocket.accept();
-                Handshake handshake = new Handshake();
-                handshake.handShakeReceived(serverS);
-
-                //Get Time Instance
-                Calendar cal = Calendar.getInstance();
-                SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-
-                System.out.println(formatter.format(cal.getTime())+" Got message from Peer with id : " + handshake.getPeerId());
-                handshake.setPeerId(this.id);
-                handshake.sendHandshakeMsg(serverS);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        if(config.getHaveFile().get(idx)){
+            BitSet bitSet = new BitSet();
+            bitSet.set(0, noOfPieces);
+            bitfieldsMap.put(id, bitSet);
+        }else{
+            bitfieldsMap.put(id, new BitSet());
         }
+
+        System.out.println("BitSet: " + bitfieldsMap.get(id));
+
+        ExecutorService clientExecutorService = Executors.newFixedThreadPool(N);
+        for (int i = 0; i < N; i++) {
+            if(i == idx) break;
+            try{
+                Socket clientSocket = new Socket(hosts.get(i), ports.get(i));
+                System.out.println("connecting to server: " + hosts.get(i) + " port: " + ports.get(i));
+                clientExecutorService.submit(new ClientHandler(clientSocket, id));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        clientExecutorService.shutdown();
+
+        ExecutorService serverExecutorService = Executors.newFixedThreadPool(N);
+        try{
+            ServerSocket serverSocket = new ServerSocket(ports.get(idx));
+            //client TCPs count
+            int TCPcount = 0;
+            while(TCPcount < N - 1 - idx){  //@TODO change logic
+                serverExecutorService.submit(new ServerHandler(serverSocket.accept()));
+                TCPcount++;
+                System.out.println("server side TCP count: " + TCPcount);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        serverExecutorService.shutdown();
+        System.out.println("shutting down Peer services " + id);
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) throws Exception{
         Peer peer = new Peer(Integer.parseInt(args[0]));
         Thread peerThread = new Thread(peer);
         peerThread.start();
     }
+
+    class ServerHandler implements Callable<String> {
+
+        private Socket socket;
+        private int clientId;
+
+        ServerHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public String call() {
+            int status = server_communication();
+            try{
+                System.out.println("closing server socket at port " + socket.getLocalPort());
+                socket.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return "status: " + status;
+        }
+
+        //return status
+        // 1 - success
+        // 0 - error
+        private int server_communication() {
+
+            try (
+                    OutputStream out = socket.getOutputStream();
+                    InputStream in = socket.getInputStream();
+
+            ) {
+
+                /*
+                    1. Handshake receive from client
+                 */
+                    Handshake handshake = new Handshake();
+                    handshake.handShakeReceived(socket);
+                    clientId = handshake.getPeerId();
+                    handshake.setPeerId(id);
+                    handshake.sendHandshakeMsg(socket);
+                    peerLog.logTcpConnected(clientId, Calendar.getInstance());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return 0;
+            }
+            return 1;
+        }
+    }
+
+    class ClientHandler implements Callable<String>{
+
+        private Socket socket;
+        private int serverId;
+
+        ClientHandler(Socket socket, int serverId){
+            this.socket = socket;
+            this.serverId = serverId;
+        }
+
+        @Override
+        public String call() {
+            int status = client_communication();
+            try{
+                System.out.println("closing client socket at port " + socket.getLocalPort());
+                socket.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return "status: " + status;
+        }
+
+        //return status
+        // 1 - success
+        // 0 - error
+        private int client_communication(){
+
+            try(
+                    OutputStream out = socket.getOutputStream();
+                    InputStream in = socket.getInputStream();
+
+            ){
+                    /*
+                        1. Handshake send to server
+                     */
+                    Handshake handshake = new Handshake();
+                    handshake.setPeerId(id);
+                    handshake.sendHandshakeMsg(socket);
+                    handShakeMap.put(serverId, false);
+                    handshake.handShakeReceived(socket);
+                    System.out.println("Handshake initiation received from " + serverId);
+                    if (handShakeMap.get(serverId) != null) {
+                        handShakeMap.put(serverId, true);
+                        peerLog.logHandshakeSuccess(serverId, Calendar.getInstance());
+                        System.out.println("Handshake Success");
+                    }
+                    peerLog.logTcpConnection(serverId, Calendar.getInstance());
+
+                    /*
+                        2. bitfield send to server
+                     */
+
+            }catch(Exception e){
+                e.printStackTrace();
+                return 0;
+            }
+            return 1;
+        }
+    }
 }
+
+
